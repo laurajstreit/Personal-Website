48fb72b58149b842a31bcccff72413c83ffdcc90
Laura Streit <lstreit@nexon.net>
2023-01-08 14:04:52 -0800

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,132 @@
+import logo from './logo.svg';
+import './App.css';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Stack from 'react-bootstrap/Stack';
+import Navbar from 'react-bootstrap/Navbar';
+import OffcanvasExample from './navbar';
+
+function Nav () {
+  return(
+    <OffcanvasExample/>
+  //   <div id="navigation">
+  //   <nav>
+  //     <ul>
+  //       <li><a href="about.html">About</a></li>
+  //       <li><a href="work.html">Work</a></li>
+  //       <li><a href="contact.html">Contact</a></li>
+  //     </ul>
+  //   </nav>
+  // </div>
+  )
+}
+
+function Title() {
+  return(
+    <Container fluid>
+      <Row>
+        <Col xs={1}/>
+        <Col id="title" style={{marginLeft:100}}>
+          <h1>Hiya, I'm Laura Streit</h1>
+          <div id="subtitle">
+            <h2>A multi-disciplinary creative working in the gaming industry.</h2>
+            <h3>I've worked in design, social media, marketing, and more!</h3>
+          </div>
+        </Col>
+      </Row>
+  </Container>
+  )
+}
+
+function GalleryCol({images}) {
+  return(
+  <Col className="g-3" xs={5}>
+  <Stack gap={3} >
+    {
+      images.map(image => {
+        return (
+          <Row>
+            <img src={"galleryImages/" + image} alt="#"/>
+          </Row>
+        )
+      })
+    }
+  </Stack >
+  </Col>
+  )
+}
+
+function Gallery () {
+  let images = [
+    "womenofesports_cover.png",
+    "transitional_combat_notext.png",
+    "htc deck cover image.png",
+    "double_insta.jpg",
+    "mute your mic behance_cover.png",
+    "theneutral_1400x1400.png",
+    // "theneutral_yt_header.jpg",
+    // "title test ugh.png",
+    // "Women_of_Esports_3.png",
+  ]
+  
+  return(
+    <Container fluid>
+      <Row>
+        <Col xs={1} />
+        <GalleryCol images={images.slice(0, images.length/2)} />
+        <GalleryCol images={images.slice(images.length/2)} />
+        <Col xs={1} />
+
+      </Row>
+    </Container>
+  )
+}
+
+function Footer () {
+  let images = [
+    "Icon awesome-behance.svg",
+    "Icon awesome-linkedin-in.svg",
+    "Icon awesome-twitter.svg",
+    "Icon metro-mail.svg",
+  ]
+  
+  return(
+		<div class="footerColumn">
+      {
+        images.map(image => {
+          return (<img src={"images/" + image} alt="#"/>)
+        })
+      }
+		</div>
+  )
+}
+
+
+function App() {
+  return (
+    <div className="App">
+      <Nav />
+      <Title />
+      <Gallery />
+      <Footer />
+      <header className="App-header">
+    
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload jhkjjhk.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/navbar.js
@@ -0,0 +1,48 @@
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import Offcanvas from 'react-bootstrap/Offcanvas';
+import lsLogo from './lsLogo.svg'
+import Col from 'react-bootstrap/Col';
+import { Row } from 'react-bootstrap';
+
+function OffcanvasExample() {
+    return (
+        <Container fluid className='mx-0 px-0'>
+            <Navbar variant="dark" bg="none"
+            // className="my-5 mx-5"
+             >
+                <Container fluid >
+                <Col xs={1}/>
+                    <Navbar.Brand href="#" >
+                        <img width="100" height="100" src={lsLogo} />
+                    </Navbar.Brand>
+                    <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-xxl`} />
+                    <Navbar.Offcanvas
+                        id={`offcanvasNavbar-expand-xxl`}
+                        aria-labelledby={`offcanvasNavbarLabel-expand-xxl`}
+                        placement="end"
+                    >
+                        <Offcanvas.Header closeButton>
+                            <Offcanvas.Title id={`offcanvasNavbarLabel-expand-xxl`}>
+                                Offcanvas
+                            </Offcanvas.Title>
+                        </Offcanvas.Header>
+                        <Offcanvas.Body>
+                            <Nav className="justify-content-end flex-grow-1 pe-3">
+                                <Nav.Link href="#action1">Home</Nav.Link>
+                                <Nav.Link href="#action2">Link</Nav.Link>
+                            </Nav>
+                        </Offcanvas.Body>
+                    </Navbar.Offcanvas>
+                    <Col xs={1}/>
+                </Container>
+            </Navbar>
+        </Container>
+    );
+}
+
+export default OffcanvasExample;
