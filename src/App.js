71c14688291cdd5064f7f66a873b06ebbf18dac9
laurajstreit <101692863+laurajstreit@users.noreply.github.com>
2023-01-08 18:37:17 -0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -122,7 +122,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React Yo
         </a>
       </header>
     </div>
